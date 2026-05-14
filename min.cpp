72d1d4900790a1ca4ad2320eb7cc29f0e43280ea
Umair Farooq <mirumair7893@gmail.com>
2026-05-14 22:43:55 +0530

    added code

--- min.cpp
+++ min.cpp
@@ -1,19 +1,20 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+void max(int arr,int n){
+    for(int i = 0; i < n; n++){
+        int max = arr[i];
 
-int main() {
-    int arr[] = {5, 2, 9, 1, 7};
-    int n = 5;
-
-    int minElement = arr[0];
-
-    for(int i = 1; i < n; i++) {
-        if(arr[i] < minElement) {
-            minElement = arr[i];
-        }
     }
 
-    cout << "Minimum element: " << minElement;
+    return max;
 
+}
+
+int main(){
+    int arr[] = {3,5,34,73,94,4,6,27,58,0,21 };
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    cout<< "maximum element ";
     return 0;
 }
+
--- selection.cpp
+++ selection.cpp
@@ -1,37 +1,3 @@
-<<<<<<< HEAD
-#include<iostream>
-#include<vector>
-using namespace std;
-
-int main(){
-    vector<int> arr = {64, 25, 12, 22, 11};
-    int n = arr.size();
-
-    for(int i = 0; i < n-1; i++){
-        int minIndex = i;
-
-        for(int j = i+1; j < n; j++){
-            if(arr[j] < arr[minIndex]){
-                minIndex = j;
-            }
-        }
-
-        // swap
-        if(minIndex != i){
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
-        }
-    }
-
-    cout << "Sorted array: ";
-    for(int i = 0; i < n; i++){
-        cout << arr[i] << " ";
-    }
-
-    return 0;
-}
-=======
 #include<iostream>
 using namespace std;
 
@@ -59,4 +25,4 @@ int main(){
     }
     return 0;
 }
->>>>>>> 9d528d184686de2cbc152689622871a984fe68ef
+
