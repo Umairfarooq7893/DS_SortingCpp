1207c611ba7b61a7369816618dc424abf8f19943
Umair Farooq <mirumair7893@gmail.com>
2026-05-15 17:31:49 +0530

    added selection sort

--- insertion.cpp
+++ insertion.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <iostream>
 using namespace std;
 
@@ -26,32 +25,4 @@ int main() {
 
     return 0;
 }
-=======
-#include <iostream>
-using namespace std;
 
-int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = 5;
-
-    for(int i = 1; i < n; i++) {
-        int key = arr[i];
-        int j = i - 1;
-
-        // Shift elements greater than key
-        while(j >= 0 && arr[j] > key) {
-            arr[j + 1] = arr[j];
-            j--;
-        }
-
-        arr[j + 1] = key;
-    }
-
-    cout << "Sorted array: ";
-    for(int i = 0; i < n; i++) {
-        cout << arr[i] << " ";
-    }
-
-    return 0;
-}
->>>>>>> 9d528d184686de2cbc152689622871a984fe68ef
--- selection.cpp
+++ selection.cpp
@@ -1,28 +1,32 @@
 #include<iostream>
 using namespace std;
+void sort(int arr[] ,int n){
+    for(int i = 0 ; i < n-1 ; i++){
+        int min = i;
 
-int main(){
-    int arr[] = {64,25,12,22,11};
-    int n = sizeof(arr)/sizeof(arr[0]);
-
-    for (int i = 0; i < n-1 ;i++){
-        int minIndex = i;
-
-        // now finding minimum element in the unsorted
-        for(int j = i+1 ; j < n ;j++){
-            if(arr[j]<arr[minIndex]){
-                minIndex = j;
-            }
+        for(int j = i+1 ; j < n ; j++){
+            if(arr[j] < arr[min])
+                min = j;
         }
-            // swaping
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+                // swap
+                int temp = arr[min];
+                arr[min] = arr[i];
+                arr[i] = temp;
+
+
     }
-    cout<<"Sorted array: ";
-    for(int i = 0; i<n ;i++){
-        cout<< arr[i]<<" ";
+
+    // printing
+    for(int i = 0 ; i < n ;i++){
+        cout<<arr[i]<<" ";
     }
-    return 0;
 }
 
+int main(){
+    int arr[] = {22,4,55,7,8,4,3,22};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    sort(arr,n);
+
+    return 0;
+}
